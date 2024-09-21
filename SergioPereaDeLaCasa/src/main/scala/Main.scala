2d008b9ee0be8cad1028c973dc2bf2389ca43e39
Sergio Perea De La Casa <sergio.perea.de.la.1@managementsolutions.com>
2024-09-21 11:18:06 +0200

    Add functionality to find and display top 100 most frequent flyers from CSV if exists

--- SergioPereaDeLaCasa/src/main/scala/Main.scala
+++ SergioPereaDeLaCasa/src/main/scala/Main.scala
@@ -1,8 +1,7 @@
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark.sql.{Column, DataFrame, Dataset, SparkSession}
 import org.apache.spark.sql.functions._
-
-import java.io.{BufferedWriter, FileWriter}
+import java.io.{BufferedWriter, FileWriter, File}
 
 object Main {
 
@@ -20,6 +19,8 @@ object Main {
     //Question 1: Find the total number of flights for each month.
     groupFlightsByDate(dataFrame = flightData, groupByFunction = month(col("date")), groupName = "Month")
 
+    //Question 2: Find the names of the 100 most frequent flyers.
+    mostFrequentFlyers(df_passengers = passengerData, df_flights = flightData)
 
     spark.stop()
   }
@@ -93,17 +94,30 @@ object Main {
 
   /**
    * Generic method to group flights by a date criterion (month, year, day, etc.)
-   * @param flightData Dataset[FlightData] Dataset of flight data
+   * @param dataFrame Dataset[FlightData] Dataset of flight data
    * @param groupByFunction Grouping function for the date column (month, year, day, etc.)
    * @param groupName Name of the group (e.g., ‘Month’, ‘Year’, etc.)
    * @return DataFrame with the total number of flights grouped by the selected criterion
    */
   private def groupFlightsByDate(dataFrame: Dataset[FlightData], groupByFunction: => Column, groupName: String): Unit = {
-    val groupedData = dataFrame.groupBy(groupByFunction.as(groupName))
-      .agg(count("flightId").as("Number of Flights"))
-      .orderBy(groupName)
+    val csvPath = "../question1.csv"
 
-    saveDataFrameAsCSV(groupedData, "../question1.csv")
+    val file = new File(csvPath)
+
+    if (file.exists()) {
+      println(s"CSV file '$csvPath' already exists. Loading data from CSV...")
+      val spark = SparkSession.builder().getOrCreate()
+      val loadedData = spark.read.option("header", "true").option("delimiter",";").csv(csvPath)
+      loadedData.show()
+    } else {
+      println(s"CSV file '$csvPath' does not exist. Generating and saving data...")
+
+      val groupedData = dataFrame.groupBy(groupByFunction.as(groupName))
+        .agg(count("flightId").as("Number of Flights"))
+        .orderBy(groupName)
+
+      saveDataFrameAsCSV(groupedData, csvPath)
+    }
   }
 
 
@@ -127,4 +141,42 @@ object Main {
     file.close()
   }
 
+
+  /**
+   * Finds and exports the top 100 most frequent flyers based on the number of flights taken.
+   *
+   * This method groups the flight data by `passengerId`, counts the number of flights per passenger,
+   * and orders the result by the passengers with the highest number of flights. The result is then
+   * joined with the passenger dataset to enrich the data with passenger details, and finally, the top
+   * 100 frequent flyers are saved to a CSV file.
+   *
+   * @param df_passengers Dataset[Passenger] containing passenger details (e.g., name, ID).
+   * @param df_flights Dataset[FlightData] containing flight data including passenger IDs.
+   */
+  private def mostFrequentFlyers(df_passengers: Dataset[Passenger], df_flights: Dataset[FlightData]): Unit = {
+    val csvPath = "../question2.csv"
+
+    val file = new File(csvPath)
+
+    if (file.exists()) {
+      println(s"CSV file '$csvPath' already exists. Loading data from CSV...")
+      val spark = SparkSession.builder().getOrCreate()
+      val loadedData = spark.read.option("header", "true").option("delimiter",";").csv(csvPath)
+      loadedData.show()
+    } else {
+      println(s"CSV file '$csvPath' does not exist. Generating and saving data...")
+
+      val freq_flyers = df_flights.groupBy("passengerId")
+        .agg(count("flightId").as("Number of Flights"))
+        .orderBy(desc("Number of Flights"))
+        .limit(100)
+        .join(df_passengers,"passengerId")
+        .orderBy(desc("Number of Flights"))
+
+      saveDataFrameAsCSV(freq_flyers,"../question2.csv")
+    }
+
+  }
 }
+
+
